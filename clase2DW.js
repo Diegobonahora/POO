54edb605ba8d8387fa8220617aa2c59e236a48c4
Diegobonahora <diegobonahora12@gmial.com>
2025-04-29 14:54:52 -0300

    Agregado nuevo archivo y cambios

---
+++ clase2DW.js
@@ -0,0 +1,8 @@
+let NombreProducto = prompt("ingrese el nombre del producto");
+let PrecioUnitario = 500;
+let CantidadDeseada = + prompt("ingrese la cantidad deseada");
+
+let PrecioTotal = CantidadDeseada * PrecioUnitario;
+
+alert(`Su total es ${PrecioTotal} por el producto ${NombreProducto}`);
+let cantidad;
